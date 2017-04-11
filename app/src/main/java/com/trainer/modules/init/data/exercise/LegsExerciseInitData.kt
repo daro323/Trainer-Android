6bc51f9d2f169b3359c1c0b9095305370d002e42
Dariusz Drozdziuk <dariusz.drozdziuk@neofonie.de>
2017-04-11 17:11:23 +0200

    Updated Legs exercise plan

--- app/src/main/java/com/trainer/modules/init/data/exercise/LegsExerciseInitData.kt
+++ app/src/main/java/com/trainer/modules/init/data/exercise/LegsExerciseInitData.kt
@@ -3,7 +3,6 @@ package com.trainer.modules.init.data.exercise
 import com.trainer.core.training.model.Exercise
 import com.trainer.core.training.model.Serie.Companion.createSet
 import com.trainer.core.training.model.Workout
-import com.trainer.modules.init.InitExerciseImageMap.*
 import com.trainer.modules.training.standard.SuperSet
 
 /**
@@ -12,43 +11,46 @@ import com.trainer.modules.training.standard.SuperSet
 class LegsExerciseInitData private constructor() {
 
   companion object {
-    private val FRONT_SQUAT = Exercise("Przysiad ze sztangą z przodu",
-        arrayListOf("Nie zbiegaj kolan środka podczas przysiadu (powinny isć na zewnątrz).",
+    private val FRONT_SQUAT = Exercise("Front Squat",
+        arrayListOf(
+            "Nie zbiegaj kolan środka podczas przysiadu (powinny isć na zewnątrz).",
             "Klata do przodu.",
-            "Łokcie do siebie na wprost i do góry."),
-        FRONT_SQUAT_IMAGE)
+            "Łokcie do siebie na wprost i do góry."))
+
+    private val WALKING_LUNGES = Exercise("Walking Lunges")
 
     private val MACHINE_LEG_PRESS = Exercise("Wyciskanie na maszynie w górę",
-        arrayListOf("W początkowej pozycji nogi ugięte pod kątem 90 stopni.",
+        arrayListOf(
+            "W początkowej pozycji nogi ugięte pod kątem 90 stopni.",
             "Wypychaj ciężar poprzez pięty.",
-            "Nie prostuj nóg do końca (niech będą lekko ugięte)."),
-        MACHINE_LEG_PRESS_IMAGE)
+            "Nie prostuj nóg do końca (niech będą lekko ugięte)."))
 
-    private val LEG_CURLS = Exercise("Uginanie nóg na maszynie",
-        arrayListOf("Oparcie ustaw tak, żeby kolano było na wysokości osi obrotu ramienia.",
-            "Stopy prosto palce wycelowane w górę."),
-        LEG_CURLS_IMAGE)
+    private val ROMANIAN_DEADLIFT = Exercise("Romanian Deadlift",
+        arrayListOf(
+            "Chwyć nachwytem na szerokość barków.",
+            "Opuszczaj bardzo powoli.",
+            "Ruch podobny do martwego tyle, że bez przysiadu.",
+            "Gdy maxymalnie rozciągniesz tyłek wykonaj dynamiczny powrót."))
 
-    private val CALF_RAISE = Exercise("Spinanie łydek siedząc",
-        arrayListOf("Wykonuj dokładnie skupiając się na pracy łydek.",
-            "Pełny zakres ruchu."),
-        CALF_RAISE_IMAGE)
+    private val LEG_CURLS = Exercise("Leg Curls",
+        arrayListOf(
+            "Oparcie ustaw tak, żeby kolano było na wysokości osi obrotu ramienia.",
+            "Stopy prosto palce wycelowane w górę."))
+
+    private val CALF_RAISE = Exercise("Calf Raise",
+        arrayListOf(
+            "Wykonuj dokładnie skupiając się na pracy łydek.",
+            "Pełny zakres ruchu."))
 
 
     val LEGS_WORKOUT = Workout(arrayListOf(
-        createSet(FRONT_SQUAT,
-            arrayListOf("Zrób 4 serie po 10, 8, 7, 6 repet."),
-            4, 90),
-        createSet(MACHINE_LEG_PRESS,
-            arrayListOf("Zrób 4 serie po 10, 8, 7, 7 repet."),
-            4, 80),
+        createSet(FRONT_SQUAT, 4, 100),
+        createSet(WALKING_LUNGES, 3, 100),
+        createSet(MACHINE_LEG_PRESS, 3, 90),
+        createSet(ROMANIAN_DEADLIFT, 2, 75),
         SuperSet(arrayListOf(
-            createSet(LEG_CURLS,
-                arrayListOf("Zrób 3 serie po 10 repet."),
-                3, 0),
-            createSet(CALF_RAISE,
-                arrayListOf("Zrób 3 serie po max repet."),
-                3, 60)
+            createSet(LEG_CURLS, 3, 0),
+            createSet(CALF_RAISE, 3, 60)
         ))
     ))
   }
