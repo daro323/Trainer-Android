a99d5b0afe8567944fdec7598271deea9ac4f4c9
Dariusz Drozdziuk <dariusz.drozdziuk@neofonie.de>
2017-04-11 16:43:02 +0200

    Updated Back exercise plan

--- app/src/main/java/com/trainer/modules/init/data/exercise/BackExerciseInitData.kt
+++ app/src/main/java/com/trainer/modules/init/data/exercise/BackExerciseInitData.kt
@@ -2,10 +2,7 @@ package com.trainer.modules.init.data.exercise
 
 import com.trainer.core.training.model.Exercise
 import com.trainer.core.training.model.Serie.Companion.createSet
-import com.trainer.core.training.model.WeightType.BODY_WEIGHT
 import com.trainer.core.training.model.Workout
-import com.trainer.modules.init.InitExerciseImageMap.DEADLIFT_IMAGE
-import com.trainer.modules.init.InitExerciseImageMap.PAUSED_PULL_UPS_IMAGE
 
 /**
  * Created by dariusz on 16/01/17.
@@ -13,40 +10,47 @@ import com.trainer.modules.init.InitExerciseImageMap.PAUSED_PULL_UPS_IMAGE
 class BackExerciseInitData private constructor() {
 
   companion object {
-    private val DEADLIFT = Exercise("Martwy ciąg",
-        arrayListOf("Opuszczaj do momentu gdy dojdziesz do połowy golenia.",
+    private val DEADLIFT = Exercise("Deadlift",
+        arrayListOf(
+            "Opuszczaj do momentu gdy dojdziesz do połowy golenia.",
             "Nie zaczynaj z przysiadu (biodra wyżej).",
             "Sztanga leży nad centralną częścią stopy.",
             "Klata do przodu i wysoko.",
             "Łopatki ściągnięte w dół i do tyłu (nie unoś ramion).",
             "Minąwszy kolana zacznij przeciągać biodra do przodu.",
             "Kolana nie mogą iść do wewnątrz.",
-            "Wypychaj do góry poprzez pięty."),
-        DEADLIFT_IMAGE)
+            "Wypychaj do góry poprzez pięty."))
 
     private val V_GRIP_ROW = Exercise("V-Grip Row")
 
-    private val PAUSED_PULL_UPS = Exercise("Podciąganie z przerwą",
-        arrayListOf("Drążek musisz minąć brodą.",
-            "Ruch zatrzymuj na 10 sekund na górze (z czasem go wydłużaj do 20).",
-            "Nogi skrzyżuj w kostkach i ugnij w kolanach.",
+    private val WEIGHTED_PULL_UPS = Exercise("Pull-Ups",
+        arrayListOf(
+            "Drążek musisz minąć brodą.",
             "Ściągaj łopatki.",
             "Chwyt szerszy niż barki.",
-            "Opuszczaj się powoli."),
-        PAUSED_PULL_UPS_IMAGE,
-        BODY_WEIGHT)
+            "Opuszczaj się powoli."))
+
+    private val BENT_OVER_ROW = Exercise("Bent Over Row",
+        arrayListOf(
+            "Pochyl się od 45 do 60 stopni.",
+            "Tułów stabilny bez ruchów.",
+            "Sztandze pozwól opadać w prostych rękach.",
+            "Przyciągaj do górnej części brzucha.",
+            "Łokcie lekko na boki.",
+            "Opuszczaj dwa razy wolniejszym ruchem.",
+            "Ściagaj łopatki na koniec ruchu."))
+
+    private val STRAIGHT_ARM_PULLDOWNS = Exercise("Straight Arm Pulldowns",
+        arrayListOf(
+            "Ściągaj łopatki wraz ze ściąganiem drążka w dół."))
 
 
     val BACK_WORKOUT = Workout(arrayListOf(
-        createSet(DEADLIFT,
-            arrayListOf("Zrób 4 serie"),
-            4, 100),
-        createSet(V_GRIP_ROW,
-            arrayListOf("Zrób 3 serie"),
-            3, 80),
-        createSet(PAUSED_PULL_UPS,
-            arrayListOf("Zrób 3 serie po max repet."),
-            3, 100)
+        createSet(DEADLIFT, 4, 100),
+        createSet(BENT_OVER_ROW, 3, 90),
+        createSet(WEIGHTED_PULL_UPS, 3, 90),
+        createSet(V_GRIP_ROW, 3, 80),
+        createSet(STRAIGHT_ARM_PULLDOWNS, 2, 70)
     ))
   }
 }
