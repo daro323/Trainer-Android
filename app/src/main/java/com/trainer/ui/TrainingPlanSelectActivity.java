8a78462ac4061c2fc2e0cccf8873ab28e3146294
Dariusz Drozdziuk <dariusz.drozdziuk@neofonie.de>
2017-08-23 18:43:33 +0200

    - Added flavors, - Created (empty) plan select activity as launcher  - wip

---
+++ app/src/main/java/com/trainer/ui/TrainingPlanSelectActivity.java
@@ -0,0 +1,14 @@
+package com.trainer.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.trainer.R;
+
+public class TrainingPlanSelectActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_training_plan_select);
+  }
+}
